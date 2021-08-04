9ac33cf8d1475123a3c3b27860dc97a842bb3cda
Juan Sanchez <juansanchezmanrique@gmail.com>
2021-08-04 11:36:01 -0500

    cambios

---
+++ src/views/VenderUsados.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="VenderUsados">
+        <div class="atras">
+            <router-link to="/">atras</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+
+.VenderUsados {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+</style>>
