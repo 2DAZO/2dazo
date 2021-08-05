74b9ddb5bb862dc0c3ea5bc93b20b3b7d58c66b1
Michael Rodriguez <maiikol1192@gmail.com>
2021-08-05 15:04:47 -0500

    Cambios

--- src/components/Navegador.vue
+++ src/components/Navegador.vue
@@ -1,129 +1,39 @@
 <template>
 <nav class="navbar navbar-expand-lg navbar-light maximo">
-<div class="container-fluid">
+  <div class="container-fluid">
     <a class="navbar-brand" href="#">
-    <img
-        src="https://res.cloudinary.com/segundazo/image/upload/v1628141649/LOGO-02_cuhfym.png"
-        alt="Logo 2DAZO"
-        class="logo"
-    />
+      <img src="https://res.cloudinary.com/segundazo/image/upload/v1628141649/LOGO_Mesa_de_trabajo_1_fwfg2h.png" alt=""  height="30">
     </a>
-    <button
-    type="button"
-    class="btn btn-primary"
-    data-bs-toggle="modal"
-    data-bs-target="#exampleModal"
-    ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
-  <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z"/>
-  <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
-</svg>
-    ingresa tu ubicación
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
     </button>
-    <div
-class="modal fade"
-id="exampleModal"
-tabindex="-1"
-aria-labelledby="exampleModalLabel"
-aria-hidden="true"
->
-<div class="modal-dialog">
-    <div class="modal-content">
-    <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button
-        type="button"
-        class="btn-close"
-        data-bs-dismiss="modal"
-        aria-label="Close"
-        ></button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item nav-link">
+          <router-link to="/" class="enlace">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-door margenes" viewBox="0 0 16 16">
+                <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5z"/>
+            </svg>
+            Inicio</router-link>
+        </li>
+        <li class="nav-item nav-link">
+          <router-link to="/" class="enlace">Categorías</router-link>
+        </li>
+        <li class="nav-item nav-link">
+          <router-link to="/" class="enlace">Lo más TOP</router-link>
+        </li>
+        <li class="nav-item nav-link">
+          <router-link to="/" class="enlace">Seguridad</router-link>
+        </li>
+        <li class="nav-item nav-link">
+          <router-link to="/" class="enlace">T&C</router-link>
+        </li>
+        <li class="nav-item nav-link">
+          <router-link to="/" class="enlace sesion">Iniciar sesión</router-link>
+        </li>
+      </ul>
     </div>
-    <div class="modal-body">...</div>
-    <div class="modal-footer">
-        <button
-        type="button"
-        class="btn btn-secondary"
-        data-bs-dismiss="modal"
-        >
-        Close
-        </button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-    </div>
-    </div>
-</div>
-</div>
-    <button
-    class="navbar-toggler"
-    type="button"
-    data-bs-toggle="collapse"
-    data-bs-target="#navbarSupportedContent"
-    aria-controls="navbarSupportedContent"
-    aria-expanded="false"
-    aria-label="Toggle navigation"
-    >
-    <span class="navbar-toggler-icon"></span>
-    </button>
-</div>
-<div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <div class="input-group">
-    <button
-        class="btn btn-outline-secondary dropdown-toggle"
-        type="button"
-        data-bs-toggle="dropdown"
-        aria-expanded="false"
-    >
-        Todas la categorías
-    </button>
-    <ul class="dropdown-menu">
-        <li><a class="dropdown-item" href="#">Action</a></li>
-        <li><a class="dropdown-item" href="#">Another action</a></li>
-        <li><a class="dropdown-item" href="#">Something else here</a></li>
-        <li><hr class="dropdown-divider" /></li>
-        <li><a class="dropdown-item" href="#">Separated link</a></li>
-    </ul>
-    <input
-        type="text"
-        class="form-control"
-        aria-label="Text input with dropdown button"
-    />
-    </div>
-    <ul class="navbar-nav mb-lg-0">
-    <li class="nav-item">
-        <router-link to="/HomeNuevo" class="nav-link enlace"
-        >Atras</router-link
-        >
-    </li>
-    <li class="nav-item">
-        <router-link to="/HomeNuevo" class="nav-link enlace"
-        >Atras</router-link
-        >
-    </li>
-    <li class="nav-item">
-        <router-link to="/HomeNuevo" class="nav-link enlace"
-        >Atras</router-link
-        >
-    </li>
-    <li class="nav-item">
-        <router-link to="/HomeNuevo" class="nav-link enlace"
-        >Atras</router-link
-        >
-    </li>
-    <li class="nav-item">
-        <router-link to="/HomeNuevo" class="nav-link enlace"
-        >Atras</router-link
-        >
-    </li>
-    <li class="nav-item">
-        <router-link to="/HomeNuevo" class="nav-link enlace"
-        >Atras</router-link
-        >
-    </li>
-    <li class="nav-item">
-        <router-link to="/HomeNuevo" class="nav-link enlace"
-        >Atras</router-link
-        >
-    </li>
-    </ul>
-</div>
+  </div>
 </nav>
 </template>
 
@@ -137,6 +47,18 @@ name: "Navegador",
 <style scoped>
 .logo {
 height: 25px;
+}
+
+.enlace{
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    color: var(--negro);
+}
+
+.margenes{
+    margin: 0 5px;
+    color: var(--naranja);
 }
 
 .maximo {
@@ -154,10 +76,6 @@ background-color: var(--gris);
 .buscador input {
 border: none;
 outline: none;
-}
-
-.enlace {
-text-decoration: none;
 }
 
 .buscador button {
