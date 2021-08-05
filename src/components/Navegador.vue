4e3120a297ea37fa8e620290c164057a6eed3e09
Michael Rodriguez <maiikol1192@gmail.com>
2021-08-04 23:59:24 -0500

    CAmbios MaicolA

---
+++ src/components/Navegador.vue
@@ -0,0 +1,32 @@
+<template lang="">
+    <div class="header">
+        <div class="logo">
+            <p class="navbar-brand">logo</p>
+        </div>
+        <div class="ubicacion">
+            <p>Ingresa tu ubicación</p>
+        </div>
+        <div class="menu">
+
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "Navegador",
+
+};
+</script>
+<style scoped>
+    .header {
+        display: flex;
+        justify-content: space-between;
+    }
+    .menu {
+        width: 40px;
+        heigth: 40px;
+        border: 1px solid gray;
+        border-radius: 50%;
+        
+    }
+</style>>
