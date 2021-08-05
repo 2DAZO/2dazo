cfbf99d4163ceeb2d6145dc993129f33f40d4abf
Michael Rodriguez <maiikol1192@gmail.com>
2021-08-05 17:39:48 -0500

    Cambios

--- src/components/Navegador.vue
+++ src/components/Navegador.vue
@@ -2,14 +2,12 @@
 <nav class="navbar navbar-expand-lg navbar-light flex-column">
   <div class="container-fluid maximo">
     <a class="navbar-brand" href="#">
-      <img src="https://res.cloudinary.com/segundazo/image/upload/v1628141649/LOGO_Mesa_de_trabajo_1_fwfg2h.png" alt=""  height="30">
+      <img src="https://res.cloudinary.com/segundazo/image/upload/v1628141649/LOGO_Mesa_de_trabajo_1_fwfg2h.png" alt=""  height="25">
     </a>
-    <button class="btn margen boton " type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-</svg>
-¿Qué estás buscando?
-  </button>
+    <div class="input-group">
+  <input type="text" class="form-control" placeholder="Lo que menos te imaginas" aria-label="Recipient's username" aria-describedby="button-addon2">
+  <button class="btn" type="button" id="button-addon2">Buscar</button>
+</div>
     <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
       <ul class="navbar-nav align-items-center">
         <li class="nav-item nav-link">
