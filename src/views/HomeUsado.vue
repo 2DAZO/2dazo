b11ec0c9d253f3e8c396fa8fd28a65a6b143215a
Juan Sanchez <juansanchezmanrique@gmail.com>
2021-08-05 00:28:18 -0500

    cambios

--- src/views/HomeUsado.vue
+++ src/views/HomeUsado.vue
@@ -5,9 +5,33 @@
       <router-link to="/nuevo">Nuevo</router-link> |
       <router-link to="/vender">Vender</router-link>
     </div>
-    <div class="banner">
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="" class="d-block w-100" alt="">
+        </div>
+        <div class="carousel-item">
+          <img src="" class="d-block w-100" alt="">
+        </div>
+        <div class="carousel-item">
+          <img src="" class="d-block w-100" alt="">
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
     </div>
-    </div>
+  </div>
 
 </template>
 
@@ -33,33 +57,10 @@ export default {
   margin: 0 20px;
 }
 
-.banner {
-  width: 100%;
-  height: 140px;
-  margin-bottom: 10px;
-  background-color: #FF7465;
-  border-radius: 10px;
-}
-
 .categorias-container {
   margin-bottom: 10px;
   display: flex;
   justify-content: space-between;
-}
-
-.categorias-container div {
-  width: 80px;
-  height: 40px;
-  background-color: #FF7465;
-  border-radius: 10px;
-}
-
-.ventas-container div {
-  width: 100%;
-  height: 140px;
-  margin-bottom: 10px;
-  background-color: #FF7465;
-  border-radius: 10px;
 }
 
 </style>
