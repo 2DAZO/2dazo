b68bdf38314e78cc76891ada0e4d0bbdc9e23ddd
Juan Sanchez <juansanchezmanrique@gmail.com>
2021-08-05 23:44:49 -0500

    cambios juan

--- src/views/HomeUsado.vue
+++ src/views/HomeUsado.vue
@@ -64,24 +64,68 @@
 
 
     <div class="container justify-content-evenly row maximo-productos">
-      <div class="card border-0 text-start" style="max-width: 300px;">
+      <div class="card border-0 text-start" style="min-width: 300px; max-width: 350px; width:100%;">
         <img src="https://http2.mlstatic.com/D_Q_NP_621621-MCO46015188216_052021-AB.webp" class="card-img-top" alt="">
         <div class="card-body d-flex flex-column justify-content-start align-items-start" style="display:grid;">
           <div>
             <p class="card-text nombre-producto"><strong>PC Master Pro PC Master Pro PC Master Pro PC Master Pro PC Master Pro</strong></p>
           </div>
-          <div class="" style="display:grid; grid-template-columns: 1fr 1fr;">
+          <div class="" style="display:grid; grid-template-columns: 1fr 1fr; margin-top:10px;">
             <div class="lh-sm" style="display: flex; flex-direction:column; justify-content:center;">
               <div style="font-size:12px;">
-                <p>Precio negociable: <strong>Si</strong></p>
+                <p style="margin-bottom:0;">Precio negociable: <strong>Si</strong></p>
               </div>
               <div>
-                <p>$ 1'700.000</p>
+                <p style="font-weight:900;">$ 1'700.000</p>
               </div>
             </div>
             <div class="lh-sm" style="text-align: end; display: flex; flex-direction:column; justify-content:center;">
-              <p>Estado</p>
-              <p>10/10</p>
+              <p style="margin-bottom:0;">Estado</p>
+              <p style="font-weight:900;">10/10</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="card border-0 text-start" style="min-width: 300px; max-width: 350px; width:100%;">
+        <img src="https://http2.mlstatic.com/D_Q_NP_621621-MCO46015188216_052021-AB.webp" class="card-img-top" alt="">
+        <div class="card-body d-flex flex-column justify-content-start align-items-start" style="display:grid;">
+          <div>
+            <p class="card-text nombre-producto"><strong>PC Master Pro PC Master Pro PC Master Pro PC Master Pro PC Master Pro</strong></p>
+          </div>
+          <div class="" style="display:grid; grid-template-columns: 1fr 1fr; margin-top:10px;">
+            <div class="lh-sm" style="display: flex; flex-direction:column; justify-content:center;">
+              <div style="font-size:12px;">
+                <p style="margin-bottom:0;">Precio negociable: <strong>Si</strong></p>
+              </div>
+              <div>
+                <p style="font-weight:900;">$ 1'700.000</p>
+              </div>
+            </div>
+            <div class="lh-sm" style="text-align: end; display: flex; flex-direction:column; justify-content:center;">
+              <p style="margin-bottom:0;">Estado</p>
+              <p style="font-weight:900;">10/10</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="card border-0 text-start" style="min-width: 300px; max-width: 350px; width:100%;">
+        <img src="https://http2.mlstatic.com/D_Q_NP_621621-MCO46015188216_052021-AB.webp" class="card-img-top" alt="">
+        <div class="card-body d-flex flex-column justify-content-start align-items-start" style="display:grid;">
+          <div>
+            <p class="card-text nombre-producto"><strong>PC Master Pro PC Master Pro PC Master Pro PC Master Pro PC Master Pro</strong></p>
+          </div>
+          <div class="" style="display:grid; grid-template-columns: 1fr 1fr; margin-top:10px;">
+            <div class="lh-sm" style="display: flex; flex-direction:column; justify-content:center;">
+              <div style="font-size:12px;">
+                <p style="margin-bottom:0;">Precio negociable: <strong>Si</strong></p>
+              </div>
+              <div>
+                <p style="font-weight:900;">$ 1'700.000</p>
+              </div>
+            </div>
+            <div class="lh-sm" style="text-align: end; display: flex; flex-direction:column; justify-content:center;">
+              <p style="margin-bottom:0;">Estado</p>
+              <p style="font-weight:900;">10/10</p>
             </div>
           </div>
         </div>
