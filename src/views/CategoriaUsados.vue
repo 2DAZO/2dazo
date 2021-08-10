cb9266f3eb9d7d567b5fbbaa2c2de72b8fe9ded1
Juan Sanchez <juansanchezmanrique@gmail.com>
2021-08-10 16:11:28 -0500

    cambios juan

---
+++ src/views/CategoriaUsados.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <Navegador></Navegador>
+    </div>
+</template>
+
+<script>
+
+import Navegador from '../components/Navegador.vue'
+
+export default {
+    name: "CategoriaUsados",
+    component: {
+        Navegador,
+    },
+}
+</script>
+
+<style >
+    
+</style>
