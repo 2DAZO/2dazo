69b54c91cfd0837a841984ee791f62fc96205d04
Michael Rodriguez <maiikol1192@gmail.com>
2021-08-11 16:22:56 -0500

    Cambios

---
+++ src/components/Banner.vue
@@ -0,0 +1,64 @@
+<template >
+<div class="row contenedor ">
+        <div id="carouselExampleIndicators" class="carousel slide col-8 me-2 rounded-3" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1">
+        </button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+        <div class="carousel-item active rounded-3">
+        <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/63b0a657792295.59e4652eb2e41.jpg" class="d-block w-100 rounded-3 " alt="Foto">
+        </div>
+        <div class="carousel-item ounded-3">
+        <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/63b0a657792295.59e4652eb2e41.jpg" class="d-block w-100 rounded-3" alt="Foto">
+        </div>
+        <div class="carousel-item ounded-3">
+        <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/63b0a657792295.59e4652eb2e41.jpg" class="d-block w-100 rounded-3" alt="Foto">
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+    </div>
+
+    <a class=" col -4 info rounded-3 me-3 position-relative cuadro-info">
+    </a>
+
+
+</div>
+
+</template>
+
+<style scoped>
+
+.contenedor{
+    width: 100%;
+    max-width: 1400px;
+    margin: auto;
+    /* max-height: 320px; */
+}
+
+.info{
+    background-color: blue;
+}
+
+.cuadro-info{
+    background: url("https://i.pinimg.com/originals/02/3a/87/023a877e31bece11178bc786e058d49f.jpg");
+    background-size: cover;
+    background-position: center;
+}
+
+.saber{
+    color: var(--gris-oscuro);
+    background-color: var(--blanco);
+    border: 1px solid var(--naranja);
+}
+
+</style>
---
+++ src/components/Categorias.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="categorias">
+        
+    </div>
+</template>
