e71ecb934f977a47d209307160ca8e86df5c85fa
Juan Sanchez <juansanchezmanrique@gmail.com>
2021-08-13 15:44:24 -0500

    cambios juan

---
+++ src/components/ListaProductos.vue
@@ -0,0 +1,148 @@
+<template>
+	<div class="d-flex container justify-content-evenly">
+		<div class="container justify-content-evenly row maximo-productos">
+			<div class="card border-0 text-start producto" >
+				<img src="https://http2.mlstatic.com/D_Q_NP_621621-MCO46015188216_052021-AB.webp" class="card-img-top" alt="">
+				<div class="card-body d-flex flex-column justify-content-start align-items-start" style="display:grid;">
+					<div>
+						<p class="card-text nombre-producto"><strong>PC Master Pro PC Master Pro PC Master Pro PC Master Pro PC Master Pro</strong></p>
+					</div>
+					<div class="" style="display:grid; grid-template-columns: 1fr 1fr; margin-top:10px;">
+						<div class="lh-sm" style="display: flex; flex-direction:column; justify-content:center;">
+							<div style="font-size:12px;">
+								<p style="margin-bottom:0;">Precio negociable: <strong>Si</strong></p>
+							</div>
+							<div>
+								<p style="font-weight:900;">$ 1'700.000</p>
+							</div>
+						</div>
+						<div class="lh-sm" style="text-align: end; display: flex; flex-direction:column; justify-content:center;">
+							<p style="margin-bottom:0;">Estado</p>
+							<p style="font-weight:900;">10/10</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container justify-content-evenly row maximo-productos">
+			<div class="card border-0 text-start producto" >
+				<img src="https://http2.mlstatic.com/D_Q_NP_621621-MCO46015188216_052021-AB.webp" class="card-img-top" alt="">
+				<div class="card-body d-flex flex-column justify-content-start align-items-start" style="display:grid;">
+					<div>
+						<p class="card-text nombre-producto"><strong>PC Master Pro PC Master Pro PC Master Pro PC Master Pro PC Master Pro</strong></p>
+					</div>
+					<div class="" style="display:grid; grid-template-columns: 1fr 1fr; margin-top:10px;">
+						<div class="lh-sm" style="display: flex; flex-direction:column; justify-content:center;">
+							<div style="font-size:12px;">
+								<p style="margin-bottom:0;">Precio negociable: <strong>Si</strong></p>
+							</div>
+							<div>
+								<p style="font-weight:900;">$ 1'700.000</p>
+							</div>
+						</div>
+						<div class="lh-sm" style="text-align: end; display: flex; flex-direction:column; justify-content:center;">
+							<p style="margin-bottom:0;">Estado</p>
+							<p style="font-weight:900;">10/10</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container justify-content-evenly row maximo-productos">
+			<div class="card border-0 text-start producto" >
+				<img src="https://http2.mlstatic.com/D_Q_NP_621621-MCO46015188216_052021-AB.webp" class="card-img-top" alt="">
+				<div class="card-body d-flex flex-column justify-content-start align-items-start" style="display:grid;">
+					<div>
+						<p class="card-text nombre-producto"><strong>PC Master Pro PC Master Pro PC Master Pro PC Master Pro PC Master Pro</strong></p>
+					</div>
+					<div class="" style="display:grid; grid-template-columns: 1fr 1fr; margin-top:10px;">
+						<div class="lh-sm" style="display: flex; flex-direction:column; justify-content:center;">
+							<div style="font-size:12px;">
+								<p style="margin-bottom:0;">Precio negociable: <strong>Si</strong></p>
+							</div>
+							<div>
+								<p style="font-weight:900;">$ 1'700.000</p>
+							</div>
+						</div>
+						<div class="lh-sm" style="text-align: end; display: flex; flex-direction:column; justify-content:center;">
+							<p style="margin-bottom:0;">Estado</p>
+							<p style="font-weight:900;">10/10</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container justify-content-evenly row maximo-productos">
+			<div class="card border-0 text-start producto" >
+				<img src="https://http2.mlstatic.com/D_Q_NP_621621-MCO46015188216_052021-AB.webp" class="card-img-top" alt="">
+				<div class="card-body d-flex flex-column justify-content-start align-items-start" style="display:grid;">
+					<div>
+						<p class="card-text nombre-producto"><strong>PC Master Pro PC Master Pro PC Master Pro PC Master Pro PC Master Pro</strong></p>
+					</div>
+					<div class="" style="display:grid; grid-template-columns: 1fr 1fr; margin-top:10px;">
+						<div class="lh-sm" style="display: flex; flex-direction:column; justify-content:center;">
+							<div style="font-size:12px;">
+								<p style="margin-bottom:0;">Precio negociable: <strong>Si</strong></p>
+							</div>
+							<div>
+								<p style="font-weight:900;">$ 1'700.000</p>
+							</div>
+						</div>
+						<div class="lh-sm" style="text-align: end; display: flex; flex-direction:column; justify-content:center;">
+							<p style="margin-bottom:0;">Estado</p>
+							<p style="font-weight:900;">10/10</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container justify-content-evenly row maximo-productos">
+			<div class="card border-0 text-start producto" >
+				<img src="https://http2.mlstatic.com/D_Q_NP_621621-MCO46015188216_052021-AB.webp" class="card-img-top" alt="">
+				<div class="card-body d-flex flex-column justify-content-start align-items-start" style="display:grid;">
+					<div>
+						<p class="card-text nombre-producto"><strong>PC Master Pro PC Master Pro PC Master Pro PC Master Pro PC Master Pro</strong></p>
+					</div>
+					<div class="" style="display:grid; grid-template-columns: 1fr 1fr; margin-top:10px;">
+						<div class="lh-sm" style="display: flex; flex-direction:column; justify-content:center;">
+							<div style="font-size:12px;">
+								<p style="margin-bottom:0;">Precio negociable: <strong>Si</strong></p>
+							</div>
+							<div>
+								<p style="font-weight:900;">$ 1'700.000</p>
+							</div>
+						</div>
+						<div class="lh-sm" style="text-align: end; display: flex; flex-direction:column; justify-content:center;">
+							<p style="margin-bottom:0;">Estado</p>
+							<p style="font-weight:900;">10/10</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+  name: "ListaProductos",
+};
+</script>
+
+<style scoped>
+.maximo-productos {
+margin: auto;
+max-width: 1200px;
+}
+.nombre-producto {
+  display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+}
+
+.producto{
+   max-width: 250px;
+   width:100%;
+}
+</style>
--- src/views/HomeUsado.vue
+++ src/views/HomeUsado.vue
@@ -2,35 +2,10 @@
   <div class="homeUsado">
     <Navegador></Navegador>
     <Banner></Banner>
-
-
     <div class="container">
       <Categorias></Categorias>
-    <div class="container justify-content-evenly row maximo-productos">
-      <div class="card border-0 text-start producto" >
-        <img src="https://http2.mlstatic.com/D_Q_NP_621621-MCO46015188216_052021-AB.webp" class="card-img-top" alt="">
-        <div class="card-body d-flex flex-column justify-content-start align-items-start" style="display:grid;">
-          <div>
-            <p class="card-text nombre-producto"><strong>PC Master Pro PC Master Pro PC Master Pro PC Master Pro PC Master Pro</strong></p>
-          </div>
-          <div class="" style="display:grid; grid-template-columns: 1fr 1fr; margin-top:10px;">
-            <div class="lh-sm" style="display: flex; flex-direction:column; justify-content:center;">
-              <div style="font-size:12px;">
-                <p style="margin-bottom:0;">Precio negociable: <strong>Si</strong></p>
-              </div>
-              <div>
-                <p style="font-weight:900;">$ 1'700.000</p>
-              </div>
-            </div>
-            <div class="lh-sm" style="text-align: end; display: flex; flex-direction:column; justify-content:center;">
-              <p style="margin-bottom:0;">Estado</p>
-              <p style="font-weight:900;">10/10</p>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
-    </div>
+    <listaProductos></listaProductos>
   </div>
 </template>
 
@@ -40,6 +15,7 @@
 import Navegador from '../components/Navegador.vue';
 import Banner from '../components/Banner.vue';
 import Categorias from '../components/Categorias.vue';
+import ListaProductos from '../components/ListaProductos.vue';
 
 export default {
   name: "HomeUsado",
@@ -47,6 +23,7 @@ export default {
     Navegador,
     Banner,
     Categorias,
+    ListaProductos,
   },
 };
 </script>
@@ -69,24 +46,9 @@ h2{
   margin-bottom: 20px;
 }
 
-.producto{
-   max-width: 250px;
-   width:100%;
-}
-
 .maximo {
 margin: auto;
 max-width: 1600px;
-}
-.maximo-productos {
-margin: auto;
-max-width: 1200px;
-}
-.nombre-producto {
-  display: -webkit-box;
-    -webkit-line-clamp: 2;
-    -webkit-box-orient: vertical;
-    overflow: hidden;
 }
 
 
