34a1af283dd0726e23f1b05c72d4d8463ac099eb
Juan Sanchez <juansanchezmanrique@gmail.com>
2021-08-21 09:11:44 -0500

    cambios juan

---
+++ src/views/Hogar.vue
@@ -0,0 +1,19 @@
+<template >
+    <div>
+      
+    </div>
+</template>
+<script>
+import Navegador from "../components/Navegador.vue";
+import ListaProductos from "../components/ListaProductos.vue";
+
+export default {
+  name: "Hogar",
+  components: {
+    Navegador,
+    ListaProductos,
+  },
+};
+</script>
+<style lang="">
+</style>
