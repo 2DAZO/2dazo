0517d9633d2c2121969c019fbea45a77c1c06125
Michael Rodriguez <maiikol1192@gmail.com>
2021-09-24 15:58:26 -0500

    Cambios

--- src/components/Barra.vue
+++ src/components/Barra.vue
@@ -91,8 +91,8 @@
     </div>
         <!-- Full screen modal -->
     <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-      <div class="modal-dialog buscador-modal">
-        <div class="modal-content">
+      <div class="modal-dialog m-0">
+        <div class="modal-content buscador-modal">
           <div class="modal-header">
             <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
@@ -152,7 +152,7 @@
   color: var(--blanco);
 }
 
-.modal-dialog{
+.buscador-modal{
   width: 100vw !important;
   height: 100vh !important;
 }
